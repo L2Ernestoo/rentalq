c64b9a51acb5fb18bf4fee38882e840d1e0a3ccf
Ernesto Orellana <jorellana@homeland.com.gt>
2022-10-28 19:24:05 -0600

    Proyecto OQ

---
+++ resources/views/index.blade.php
@@ -0,0 +1,21 @@
+@extends('plantilla')
+
+@section('contenido')
+<body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<div>
+    <h1 class="text-center">ALQUILERQ - SQA</h1>
+</div>
+<div class="d-flex mt-4 justify-content-center">
+    <img  src="dist/img/LogoAgencia.jpg" style="opacity: .7; width: 300px; height: 150px;">
+    <br>
+    <br>
+</div>
+
+</body>
+
+@endsection
